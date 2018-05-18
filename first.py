f5355718f82cee5a871e9c40152523b8b84d19f8
Michael Braun <michaelbraunbass@gmail.com>
2018-05-18 16:45:00 -0600

    clean up

--- first.py
+++ first.py
@@ -64,7 +64,7 @@ f.write('upload_info="true" \n')
 f.close()
 
 #cronjob to have next python file run on system-startup
-<<<<<<< HEAD
+
 def subprocess_cmd(command):
 	process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
 	proc_stdout = process.communicate()[0].strip()
@@ -72,9 +72,3 @@ def subprocess_cmd(command):
 cmd1 =""" "add cron job pythonrestart command '$FWDIR/Python/bin/python /mnt/usb-storage/second.py' recurrence system-startup" """
 cmd2 =""" "config_system -f /mnt/usb-storage/first_time_wizard.txt" """ 
 subprocess_cmd("clish -c " + cmd1 + "; clish -c" + cmd2)
-#subprocess.Popen("add cron job python-restart command python /mnt/BlinkPlugAndPlay_usb/second.py recurrence system-startup")
-#subprocess.Popen("config-system -f /mnt/BlinkPlugAndPlay_usb/first_time_wizard.txt ")
-=======
-subprocess.Popen("add cron job python-restart command python /mnt/BlinkPlugAndPlay_usb/second.py recurrence system-startup")
-subprocess.Popen("config-system -f /mnt/BlinkPlugAndPlay_usb/first_time_wizard.txt ")
->>>>>>> fa8066e0ef5fad84c2009fcb55105ddc818d7b33
