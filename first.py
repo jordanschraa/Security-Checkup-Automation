5d71ef00507847bd29c2dcf8977da7d1c507e49c
jordanschraa <jordan.schraa@gmail.com>
2018-05-18 14:51:44 -0600

    first+second

--- first.py
+++ first.py
@@ -2,13 +2,11 @@
 #Install Gaia
 #Insert USB, Set expert password, enter expert mode and run CPCheckupConfig.py. Include COntract File
 
-
 #Import Modules
 import getpass
 import subprocess
 import time
 import urllib2
-
 
 #Methods
 def test_connectivity():
@@ -30,29 +28,40 @@ print "                                                | |                      
 print "                                                |_|                              |___/ "
 print "Written by Mike Braun and Jordan Schraa \n\n"
 
-
 #Get Variables
-#management_IP = raw_input("Enter desired Management IP: ")
+management_IP = raw_input("Enter desired Management IP: ")
 
 print "Enter desired GAIA password"
 password = getpass.getpass()
 
-#license_string = raw_input ("Enter License String (Do not include cplic put): ")
+license_string = raw_input ("Enter License String (Do not include cplic put): ")
 
-'''
 client_IP = raw_input ("Enter desired Client IP(Optional): ")
 if client_IP is "":
 	client_interface = raw_input ("Enter desired Client Interface(Default is eth5): ")
 	if client_interface is "":
 		client_interface = "eth5"
-'''
+
 dns_IP = raw_input ("Enter Desired DNS Server(Optional - Default is 8.8.8.8): ")
 if dns_IP is "":
 	dns_IP = "8.8.8.8"
 
 #Set Default CLI to Bash
 #subprocess.Popen("chsh -s /bin/bash admin")
+						
+f = open('first_time_wizard.txt', "w+")
+f.write('hostname=security_checkup \n')
+f.write('mgmt_admin_name=admin \n')
+f.write('mgmt_admin_passwd='+password+' \n')
+f.write('mgmt_gui_clients_radio=any \n')
+f.write('install_security_managment="true" \n')
+f.write('install_security_gw="true" \n')
+f.write('install_mgmt_primary="true" \n')
+f.write('install_mgmt_secondary="false" \n')
+f.write('download_info="true" \n')
+f.write('upload_info="true" \n')
+f.close()
 
-#Run First Time Config Wizard Tasks
-#Will have to set a onetime startup script to start after the reboot
-
+#cronjob to have next python file run on system-startup
+subprocess.Popen("add cron job python-restart command python /mnt/BlinkPlugAndPlay_usb/second.py recurrence system-startup")
+subprocess.Popen("config-system -f /mnt/BlinkPlugAndPlay_usb/first_time_wizard.txt ")
---
+++ second.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.Popen("touch security_test.txt")
