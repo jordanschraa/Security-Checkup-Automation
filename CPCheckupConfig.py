f58336b92e227b63716b6500f0c991df23e6ab17
jordanschraa <jordan.schraa@gmail.com>
2018-05-23 09:21:32 -0600

    restore

--- CPCheckupConfig.py
+++ CPCheckupConfig.py
@@ -56,3 +56,44 @@ if dns_IP is "":
 #Run First Time Config Wizard Tasks
 #Will have to set a onetime startup script to start after the reboot
 
+
+#Gaia Setup
+#Run Modify Script
+subprocess.Popen("./modifyScript.sh")
+time.sleep(5)
+
+#Configure Interface
+subprocess.Popen("clish")
+subprocess.Popen("add dhcp client interface " + client_interface)
+subprocess.Popen("set interface state on")
+
+#Test Internet Connection
+internet = False
+
+if test_connectivity():
+	internet = True
+	continue
+else:
+	print "No Internet Connection. Updates and License Activation will fail an will have to be done manually"
+	continue
+
+#Active License
+subprocess.Popen("cplic put " + license_string)
+#May have to have contract file downloaded and on usb. Then subprocess can be used to call file
+
+#Update Gaia. Download and update every hotfix available
+for x in range (10)
+	subprocess.Popen("installer download-and-install " + x)
+	time.sleep(5)
+	subprocess.Popen("s")
+#
+
+#Smart Console Setup
+
+#Login
+subprocess.Popen("mgmt login")
+time.sleep(2)
+ 
+
+#Get Topology???
+
